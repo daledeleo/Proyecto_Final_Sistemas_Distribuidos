d20b8c844ef39f51f5ebe6ada8079eb2d1ca014a
daledeleo <dalede@espol.edu.ec>
2020-08-27 09:47:07 -0500

    completando de añadir el proyecto

--- HelpIn/routes/autenticacion.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var usersController = require('../controllers/user.controller')
-
-app.post('/auth', [
-       VerifyUserMiddleware.hasAuthValidFields,
-       VerifyUserMiddleware.isPasswordAndUserMatch,
-       AuthorizationController.login
-   ]);
