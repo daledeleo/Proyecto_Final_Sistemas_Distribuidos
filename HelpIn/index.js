26e9bc83042b19fc0d3ad9931c8ff5ee60a1592f
Fabricio Herrera <fabricio@MacBook-Pro.local>
2020-08-27 21:11:43 -0500

    Mostrando texto en front - solo texto de prueba load balancing por defecto

--- HelpIn/index.js
+++ HelpIn/index.js
@@ -20,5 +20,7 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(`<h1>${process.env.MESSAGE}</h1>`);
+  res.end(`<h1>Proyecto Final de Sistemas Distribuidos</h1>
+           <h2>${process.env.MESSAGE}</h2> 
+           <h2>Corriendo en puerto 8080</h2>`);
 }).listen(8080);
